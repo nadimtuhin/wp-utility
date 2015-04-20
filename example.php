613e85ae8e6d57e5070d7bb19ba3fc762b5ad72b
Nadim Tuhin <nadimtuhin@gmail.com>
2015-04-20 16:34:09 +0600

    added example

---
+++ example.php
@@ -0,0 +1,16 @@
+<?php
+require "vendor/autoload.php";
+
+use ThemeXpert\WpUtility\Metabox;
+
+class XBox extends Metabox{
+	public function render($post){
+		echo view(__DIR__ . "/views/boxer-settings.php", array("name"=>"Tuhin"));
+	}
+
+	public function save($post_id){
+		echo "saving.."; die();
+	}
+}
+
+$box = new XBox("ps", "Page Sections", ["page_section"]);
